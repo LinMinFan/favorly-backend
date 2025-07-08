0a82bd7b8315a3fee4dc73bb4b30b1c537f08e04
jacklin <jack.lin@hcinnovation.tw>
2025-07-08 14:39:39 +0800

    first commit

---
+++ src/test/java/com/example/favorly_backend/FavorlyBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.favorlybackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FavorlyBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
